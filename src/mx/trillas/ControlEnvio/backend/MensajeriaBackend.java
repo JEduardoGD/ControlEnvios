b1b0b210f2cb886c562c03e3f11d669f9a336f1d
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2016-05-03 09:41:57 -0500

    Se agrega un logger

--- src/mx/trillas/ControlEnvio/backend/MensajeriaBackend.java
+++ src/mx/trillas/ControlEnvio/backend/MensajeriaBackend.java
@@ -13,41 +13,42 @@ public class MensajeriaBackend {
 
 	 private static final String STRING_PATTERN = "([a-zA-ZpáéíóúÁÉÍÓÚ\\s]){3,45}";
 	 private static final String RIGHT_PATTERN = "^[a-zA-Z]*";
-	 
+
 	 private static Logger logger = Logger.getLogger(MensajeriaBackend.class);
-	 
+
 	 private static MensajeriaDAO mensajeriaDAO = new MensajeriaDAODBImpl();
-	 
+
 	 public static boolean checkString(String phrase){
-		  
+
 		  Pattern pattern;
 		  Matcher matcher;
-	 
+
 		  pattern = Pattern.compile(STRING_PATTERN);
 		  matcher = pattern.matcher(phrase);
-		  
+
 		  return matcher.matches();
 	 }
 	  public static boolean checkRightString(String phrase){
-		  
+
 		  Pattern pattern;
 		  Matcher matcher;
-	 
+
 		  pattern = Pattern.compile(RIGHT_PATTERN);
 		  matcher = pattern.matcher(phrase);
-		  
+
 		  return matcher.matches();
 	 }
-	  
+
 	 public static void loadMensajeriaData(String nombreMensajeria) throws Exception{
 		 Mensajeria mensajeria = null;
 		 mensajeria  = new Mensajeria();
-		 
+
 		 mensajeria.setNombre(nombreMensajeria);
-		 
+
 		 try {
 			 mensajeriaDAO.altaMensajeria(mensajeria);
 		} catch (Exception e) {
+			logger.error(e.getMessage());
 			throw e;
 		}
 	 }
